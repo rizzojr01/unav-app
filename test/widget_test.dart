17e201d5cfb2201042495832c7eb790d269e9368
Bibek Timilsina <bibektimilsina444@gmail.com>
2026-03-26 01:15:19 +0545

    test: delete UNav app boot widget test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:unav_app/main.dart';
-
-void main() {
-  testWidgets('UNav app boots', (tester) async {
-    await tester.pumpWidget(const UNavApp());
-
-    expect(find.text('UNav Navigation'), findsNothing);
-  });
-}
